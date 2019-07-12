b1829d01de17929c63c926697e1859a8a2c98df3
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-07-12 11:37:36 +0900

    Test chunked wip

---
+++ __tests__/chunked-dc.test.js
@@ -0,0 +1,42 @@
+import { chunked } from "../lib";
+// import { connectDC } from "./utils";
+
+let dc;
+let cdc;
+beforeEach(() => {
+  const pc = new RTCPeerConnection();
+  dc = pc.createDataChannel("x");
+  cdc = chunked(dc);
+});
+afterEach(() => {
+  cdc.close();
+  dc = cdc = null;
+});
+
+// should not send in parallel
+// should send only blob or file
+// should ignore empty
+
+fdescribe("ChunkedDataChannel#constructor()", () => {
+  it("should not change binaryType", () => {
+    // default
+    expect(cdc.binaryType).toBe("arraybuffer");
+    expect(() => (cdc.binaryType = "blob")).toThrowError(/change/);
+    // not changed
+    expect(cdc.binaryType).toBe("arraybuffer");
+  });
+});
+
+fdescribe("ChunkedDataChannel#send()", () => {
+  it("should return Promise<T>", () => {
+    expect(cdc.send() instanceof Promise).toBeTruthy();
+  });
+
+  it("should throw before open", async () => {
+    try {
+      await cdc.send("hi");
+    } catch (err) {
+      expect(err).toMatch(/Not open/);
+    }
+  });
+});
